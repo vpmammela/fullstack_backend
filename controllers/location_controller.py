6b8ab3fb5749b80bd1b824653a627f91c9bf49f5
vmammela <veli-pekka.mammela@edu.lapinamk.fi>
2023-11-26 13:41:51 +0200

    add auth to create_location

--- controllers/location_controller.py
+++ controllers/location_controller.py
@@ -2,12 +2,15 @@ from fastapi import APIRouter, Depends
 from starlette.responses import Response
 from fastapi.security import OAuth2PasswordRequestForm
 from fullstack_token.session import backend, cookie, verifier
-from fastapi import Depends
+from fastapi import Depends, HTTPException
 from pydantic import BaseModel
 from typing import Annotated
 import models
 from dtos.location import CreateLocationReq, CreateLocationResp
 from services.location_sqlalchemy import LocationService
+from services.auth_sqlalchemy import AuthService, AuthServ
+from dependencies import LoggedInUser
+
 
 router = APIRouter(
     tags=['location'],
@@ -18,6 +21,9 @@ LoginForm = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 
 @router.post('/location', dependencies=[Depends(cookie)], response_model=CreateLocationResp)
-async def create_location(req: CreateLocationReq, service: LocationService = Depends(LocationService)):
-    location = service.create(req)
-    return CreateLocationResp(id=location.id, name=location.name)
+async def create_location(req: CreateLocationReq, authService: AuthServ, account: LoggedInUser, service: LocationService = Depends(LocationService)):
+    if account.role != "admin":
+        raise HTTPException(status_code=401, detail='unauthorized')
+    else:
+        location = service.create(req)
+        return CreateLocationResp(id=location.id, name=location.name)
