c61cf8ba64c7e48649ff0cdc00c9b165a13cffc4
MirkaHienonen <mhienone@edu.lapinamk.fi>
2023-12-09 15:23:16 +0200

    trying to get old reports

--- services/inspectionresult_sqlalchemy.py
+++ services/inspectionresult_sqlalchemy.py
@@ -1,4 +1,4 @@
-import dtos.inspectionresult
+"""import dtos.inspectionresult
 import models
 from services.base_service import BaseService
 
@@ -19,3 +19,30 @@ class InspectionResultService(BaseService):
         self.db.commit()
 
         return result
+        """
+    
+import dtos.inspectionresult
+import models
+from services.base_service import BaseService
+from typing import List
+
+class InspectionResultService(BaseService):
+    def __init__(self, db: models.Db):
+        super(InspectionResultService, self).__init__(db)
+
+    def create(self, req: dtos.inspectionresult.CreateInspResReq, createdAt):
+        result = models.Inspectionresult(
+            createdAt=createdAt,
+            value=req.value,
+            note=req.note,
+            title=req.title,
+            inspectionform_id=req.inspectionform_id,
+        )
+
+        self.db.add(result)
+        self.db.commit()
+
+        return result
+
+    def get_inspection_results_by_form_id(self, inspection_form_id: int) -> List[models.Inspectionresult]:
+        return self.db.query(models.Inspectionresult).filter_by(inspectionform_id=inspection_form_id).all()
