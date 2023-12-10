bb9d9236106964dcc3ff14965ce65222d9ed5f41
vmammela <veli-pekka.mammela@edu.lapinamk.fi>
2023-12-10 18:35:07 +0200

    minor fixes to report_controller

--- controllers/report_controller.py
+++ controllers/report_controller.py
@@ -21,23 +21,22 @@ LoginForm = Annotated[OAuth2PasswordRequestForm, Depends()]
 
 
 @router.get('/report/environment/{id}/{inspectionType}', dependencies=[Depends(cookie)], response_model=InspectionresultListResponse)
-async def get_report_by_environment_id(id: int, inspectionType: str, account: LoggedInUser, service: ReportService = Depends(ReportService)):
+async def get_report_by_environment_id_and_inspection_type(id: int, inspectionType: str, account: LoggedInUser, service: ReportService = Depends(ReportService)):
     reports = service.get_by_environment_id(id, inspectionType)
     users_resp_list = [
         {
             "id": result.id,
             "note": result.note,
             "inspectionform_id": result.inspectionform_id,
-            "createdAt": result.createdAt.isoformat(),  # Convert datetime to string
+            "createdAt": result.createdAt.isoformat(),
             "value": result.value,
             "title": result.title,
             "inspectionform": {
-                "createdAt": result.inspectionform.createdAt.isoformat(),  # Convert datetime to string
+                "createdAt": result.inspectionform.createdAt.isoformat(),
                 "user_id": result.inspectionform.user_id,
                 "inspectiontarget_id": result.inspectionform.inspectiontarget_id,
                 "id": result.inspectionform.id,
                 "closedAt": result.inspectionform.closedAt.isoformat() if result.inspectionform.closedAt else None,
-                # Convert datetime to string or None
                 "environment_id": result.inspectionform.environment_id,
                 "inspectiontype_id": result.inspectionform.inspectiontype_id
             }
@@ -45,6 +44,6 @@ async def get_report_by_environment_id(id: int, inspectionType: str, account: Lo
         for result in reports
     ]
 
-    return InspectionresultListResponse(items=users_resp_list)
+    return InspectionresultListResponse(reports=users_resp_list)
 
 
