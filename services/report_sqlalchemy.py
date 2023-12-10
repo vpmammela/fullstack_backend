0207d10e3b31146073b839027e691b93ff62fc40
vmammela <veli-pekka.mammela@edu.lapinamk.fi>
2023-12-10 21:07:20 +0200

    add /report/{inspectionType and clean up code

--- services/report_sqlalchemy.py
+++ services/report_sqlalchemy.py
@@ -38,3 +38,37 @@ class ReportService(BaseService):
         )
 
         return inspection_results
+
+    def get_by_inspectiontype(self, inspectionType: str):
+        inspection_results = (
+            self.db.query(models.Inspectionresult)
+                .join(models.Inspectionform)
+                .join(models.Inspectiontype)
+                .filter(models.Inspectiontype.name == inspectionType)
+                .options(joinedload(models.Inspectionresult.inspectionform))
+                .all()
+        )
+
+        return inspection_results
+
+    def generate_reports_resp_list(self, reports):
+        return [
+            {
+                "id": result.id,
+                "note": result.note,
+                "inspectionform_id": result.inspectionform_id,
+                "createdAt": result.createdAt.isoformat(),
+                "value": result.value,
+                "title": result.title,
+                "inspectionform": {
+                    "createdAt": result.inspectionform.createdAt.isoformat(),
+                    "user_id": result.inspectionform.user_id,
+                    "inspectiontarget_id": result.inspectionform.inspectiontarget_id,
+                    "id": result.inspectionform.id,
+                    "closedAt": result.inspectionform.closedAt.isoformat() if result.inspectionform.closedAt else None,
+                    "environment_id": result.inspectionform.environment_id,
+                    "inspectiontype_id": result.inspectionform.inspectiontype_id
+                }
+            }
+            for result in reports
+        ]
